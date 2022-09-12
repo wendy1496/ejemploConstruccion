203608e966b3050f3f2b18d89b2e2bb132048b5d
Juan Esteban Cañas Garcia <zapatalucas643@gmail.com>
2022-09-12 15:18:53 -0500

    Modifique una linea

--- src/ejemplogithub/EjemploGitHub.java
+++ src/ejemplogithub/EjemploGitHub.java
@@ -15,7 +15,8 @@ public class EjemploGitHub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola, solo estamos usando GIT");
+        System.out.println("Hola,");
+        System.out.println(" Aprenda a programar");
     }
     
 }
